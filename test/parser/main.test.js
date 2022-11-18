d2bd43c4b13fd5d6ed6e7c98072c57a8de20f115
Dingding Zhang <dingding.zhang@meri.co>
2022-11-18 17:51:09 +0800

    feat: update test parser

--- test/parser/main.test.js
+++ test/parser/main.test.js
@@ -1,26 +1,56 @@
+import path from "path";
+import dotenv from "dotenv";
 import { expect } from "@hapi/code";
 import * as Lab from "@hapi/lab";
 
+import {
+  MARKDOWN_SOURCE_INFO_FILE,
+  getMarkdownManifest,
+  readYamlFile,
+  isFileDirValid,
+} from "../../src/misc.js";
 import { getAllDocsForPublishing, getAllDocsForServing } from "../../src/parser/main.js";
 
 const lab = Lab.script();
 const { suite, test, before } = lab;
 
+function getMarkdownSourceInfo() {
+  const { manifestFilePath, manifest } = getMarkdownManifest();
+  if (!manifestFilePath || !Array.isArray(manifest) || manifest.length < 1) {
+    return null;
+  }
+  const { dir: manifestFileDir } = path.parse(manifestFilePath);
+  const markdownSourcePath = path.resolve(manifestFileDir, manifest[0].path);
+  return readYamlFile(path.join(markdownSourcePath, MARKDOWN_SOURCE_INFO_FILE));
+}
+
 suite("Testing Parser", () => {
+  let markdownSourceKey;
   let docsForPublishing;
   let docsForServing;
 
   before(() => {
-    docsForPublishing = getAllDocsForPublishing();
-    docsForServing = getAllDocsForServing();
+    dotenv.config();
+    const markdownSourceInfo = getMarkdownSourceInfo();
+    markdownSourceKey = markdownSourceInfo?.key;
+    if (isFileDirValid(markdownSourceKey)) {
+      docsForPublishing = getAllDocsForPublishing();
+      docsForServing = getAllDocsForServing();
+    }
+  });
+
+  test("markdown source should exist and have a valid key", () => {
+    const isMarkdownSourceKeyValid = isFileDirValid(markdownSourceKey);
+    expect(isMarkdownSourceKeyValid).to.be.a.boolean().and.to.equal(true);
   });
 
   test("getAllDocsForPublishing", () => {
     expect(docsForPublishing).to.be.an.array();
     expect(docsForPublishing.length).to.be.at.least(1);
 
-    const doc = docsForPublishing.find(item => item.relativePath === "en/test@3.html");
+    const doc = docsForPublishing.find(item => item.relativePath === `${markdownSourceKey}/en/test@3.html`);
     expect(doc).to.exist();
+    expect(doc.source).to.equal(markdownSourceKey);
     expect(doc.lang).to.equal("en");
     expect(doc.edtion).to.equal(3);
     expect(doc.info.title).to.equal("Manchester by the Sea V3");
@@ -32,8 +62,9 @@ suite("Testing Parser", () => {
   test("getAllDocsForServing", () => {
     expect(docsForServing).to.be.an.object();
 
-    const doc = docsForServing["en/test.html"];
+    const doc = docsForServing[`${markdownSourceKey}/en/test.html`];
     expect(doc).to.exist();
+    expect(doc.source).to.equal(markdownSourceKey);
     expect(doc.lang).to.equal("en");
     expect(doc.edtion).to.be.null();
     expect(doc.info.title).to.equal("Manchester by the Sea");
